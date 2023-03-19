eda5ba649bd2fefdc9682947e1a898a9d75539b9
Phakarat <Phakarat.kh@ku.th>
2023-03-19 20:23:22 +0700

    Integrate custom login form and OAuth login

--- src/main/java/ku/kinkao/config/SecurityConfig.java
+++ src/main/java/ku/kinkao/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ku.kinkao.config;
 
 import ku.kinkao.service.UserDetailsServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -19,6 +22,12 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsServiceImp userDetailsService;
+
+    @Autowired
+    private OidcUserService oidcUserService;
+
+    @Autowired
+    private ApplicationContext context;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -48,6 +57,15 @@ public class SecurityConfig {
                 .deleteCookies("JSESSIONID", "remember-me")
                 .permitAll();
 
+        ClientRegistrationRepository repository =
+                context.getBean(ClientRegistrationRepository.class);
+
+        if (repository != null) {
+            http
+                    .oauth2Login().clientRegistrationRepository(repository)
+                    .userInfoEndpoint().oidcUserService(oidcUserService).and()
+                    .loginPage("/login").permitAll();
+        }
         return http.build();
     }
 

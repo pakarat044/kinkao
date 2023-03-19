ab98c6a1e7efa6cf751a3d93c4a2aff596d9e287
Phakarat <Phakarat.kh@ku.th>
2023-03-19 22:30:02 +0700

    Add Random ID to Model classes

--- src/main/java/ku/kinkao/model/Restaurant.java
+++ src/main/java/ku/kinkao/model/Restaurant.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
@@ -14,8 +15,8 @@ import javax.persistence.Id;
 public class Restaurant {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue
+    private UUID id;
 
     private String name;
     private String address;
--- src/main/java/ku/kinkao/repository/MemberRepository.java
+++ src/main/java/ku/kinkao/repository/MemberRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, UUID> {
 
     // @Query("SELECT * FROM List<Member> findSomexxx(...)")
     Member findByUsername(String username);

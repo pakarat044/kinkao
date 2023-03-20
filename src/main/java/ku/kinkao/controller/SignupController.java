d341d9aa47aac1da4e1dce091825116a39d77a34
Phakarat <Phakarat.kh@ku.th>
2023-03-20 21:06:18 +0700

    Input Validation

--- src/main/java/ku/kinkao/controller/SignupController.java
+++ src/main/java/ku/kinkao/controller/SignupController.java
@@ -6,9 +6,12 @@ import ku.kinkao.service.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class SignupController {
@@ -17,12 +20,15 @@ public class SignupController {
     private SignupService signupService;
 
     @GetMapping("/signup")
-    public String getSignupPage() {
+    public String getSignupPage(SignupDto user) {
         return "signup"; // return signup.html
     }
 
     @PostMapping("/signup")
-    public String signupMember(@ModelAttribute SignupDto member, Model model){
+    public String signupMember(@Valid SignupDto member, BindingResult result, Model model){
+
+        if  (result.hasErrors())
+            return "signup";
 
         String signupError = null;
         if (!signupService.isUsernameAvailable(member.getUsername())) {
@@ -41,6 +47,7 @@ public class SignupController {
             model.addAttribute("signupError", signupError);
         }
 
+        model.addAttribute("signupDto", new SignupDto());
         return "signup";
     }
 

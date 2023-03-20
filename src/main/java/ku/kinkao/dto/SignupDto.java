fe7ba18dc1d6adf3bb70adc08326419aae1ac50f
Phakarat <Phakarat.kh@ku.th>
2023-03-20 22:04:10 +0700

    Password Validation

--- src/main/java/ku/kinkao/dto/SignupDto.java
+++ src/main/java/ku/kinkao/dto/SignupDto.java
@@ -1,5 +1,6 @@
 package ku.kinkao.dto;
 
+import ku.kinkao.validation.ValidPassword;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
@@ -15,7 +16,8 @@ public class SignupDto {
     private String username;
 
     @NotBlank
-    @Size(min=12, max=128, message = "Password must have at least 12 characters")
+    @ValidPassword
+    //@Size(min=12, max=128, message = "Password must have at least 12 characters")
     private String password;
 
     @NotBlank(message = "First name is required")
